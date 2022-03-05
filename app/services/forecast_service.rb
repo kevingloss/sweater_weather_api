b9edf0b0d58a5f0e61ef9554f2e2ed942648b152
Kevin Gloss <kgloss89@gmail.com>
2022-03-05 14:23:24 -0700

    Create forecast service

---
+++ app/services/forecast_service.rb
@@ -0,0 +1,19 @@
+class ForecastService < ApiService 
+  class << self 
+    def connection
+      response = Faraday.new(url: 'https://api.openweathermap.org/data/2.5/') do |f|
+        f.params['appid'] = ENV['weather_key']
+      end
+    end
+
+    def get_forecast(lat, lon)
+      response = connection.get('onecall') do |f|
+        f.params['lat'] = lat 
+        f.params['lon'] = lon
+        f.params['exclude'] = 'minutely,alerts'
+      end
+
+      parse_json(response)
+    end
+  end
+end
