ba9fa47637a1aa50e3ccf612006f170ee78a65ad
Kevin Gloss <kgloss89@gmail.com>
2022-03-06 13:22:09 -0700

    Add user create/login routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
       resources :forecast, only: :index
-      resources :backgrounds , only: :index
+      resources :backgrounds, only: :index
+      resources :users, only: :create
+      resources :sessions, only: :create
     end
   end
 end
