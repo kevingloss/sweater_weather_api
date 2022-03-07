c35ce906bebb544bcda43747043f975bc4e4f6e4
Kevin Gloss <kgloss89@gmail.com>
2022-03-07 11:17:58 -0700

    Create book-search endpoint

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::BooksController < ApplicationController
+  def index 
+    if params[:location].present? && params[:quantity].present?
+      coords = MapFacade.get_coords(params[:location])
+      current_forecast = ForecastFacade.get_forecast(coords.lat, coords.lon).current
+      book_details = BookFacade.get_books(params[:location], params[:quantity])
+      destination_books = BookSearch.new(book_details, current_forecast, params[:location])
+      json_response(BooksSerializer.new(destination_books))
+    else
+      render json: {error: 'Please enter valid location and quantity.'}, status: :bad_request
+    end
+  end
+end
