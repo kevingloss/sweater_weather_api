319ff9f04b265dce4b20bc144c99b912123a1ebc
Kevin Gloss <kgloss89@gmail.com>
2022-03-06 20:49:27 -0700

    Create user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  has_secure_password
-  validates_presence_of :password_digest
-  validates_presence_of :email
+  has_many :api_keys
+  validates_presence_of :password_digest, :email
   validates :email, email: true, uniqueness: true
-  has_many :api_keys, as: :bearer
+  has_secure_password
+
+  before_save { email.try(:downcase!) }
 end
