057229ea1d6fd38f3aba79b3a2c3a3820cc57c5a
Kevin Gloss <kgloss89@gmail.com>
2022-03-06 20:47:45 -0700

    Create placeholder for login testing

--- spec/requests/api/v1/sessions_request_spec.rb
+++ spec/requests/api/v1/sessions_request_spec.rb
@@ -1,11 +1,11 @@
-require 'spec_helper'
+require 'rails_helper'
 
-RSpec.describe "Session" do
-  it "user can log in" do
-    user = User.create!(email: 'userre@gmail.com', password: '123', password_confirmation: '123')
+RSpec.describe "Session API Request", type: :request do
+  xit "user can log in" do
+    user = User.create!(email: 'user@gmail.com', password: '123', password_confirmation: '123')
     headers = { 'CONTENT_TYPE' => 'application/json' }
 
-    post api_v1_sessions_path, headers: headers, params: {email: user.email, password: user.password }
+    post api_v1_sessions_path, headers: headers, params: JSON.generate(user: {email: user.email, password: user.password })
 
     response.status.should be(201)
   end
