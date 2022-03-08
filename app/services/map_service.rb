72c2da457ae5bbfdac5204654402a4d27b9ba351
Kevin Gloss <kgloss89@gmail.com>
2022-03-08 10:14:04 -0700

    Create route service

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -1,14 +1,23 @@
 class MapService < ApiService
   class << self
     def connection
-      response = Faraday.new(url: 'http://www.mapquestapi.com/geocoding/v1/') do |f|
+      response = Faraday.new(url: 'http://www.mapquestapi.com/') do |f|
         f.params['key'] = ENV['map_quest_key']
       end
     end
 
     def get_coords(location)
-      response = connection.get('address') do |f|
+      response = connection.get('geocoding/v1/address') do |f|
         f.params['location'] = location
+      end
+
+      parse_json(response)
+    end
+
+    def get_route(from, to)
+      response = connection.get('directions/v2/route') do |f|
+        f.params['from'] = from
+        f.params['to'] = to 
       end
 
       parse_json(response)
