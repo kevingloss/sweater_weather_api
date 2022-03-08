88e4d2464ea9a49249d0cad658713d38751e6230
Kevin Gloss <kgloss89@gmail.com>
2022-03-07 18:06:50 -0700

    Create login endpoint

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,16 +2,11 @@ class Api::V1::SessionsController < ApplicationController
   include ApiKeyAuthenticatable 
 
   def create
-    authenticate_with_http_basic do |email, password| 
-      user = User.find_by email: email 
-      # binding.pry
-      if user&.authenticate(password) 
-        api_key = user.api_keys.create! token: SecureRandom.hex 
-        # binding.pry
-        render json: api_key, status: :created and return 
-      end 
+    user = User.find_by(email: params[:email])
+    if user&.authenticate(params[:password])
+      json_response(UsersSerializer.new(user), :ok)
+    else
+      json_response({error: 'Please enter valid email and or password.'}, :bad_request)
     end
- 
-    render status: :unauthorized
   end
 end
