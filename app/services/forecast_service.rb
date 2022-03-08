70a96508fc8d4c5077769a7cd0108d6f676144a8
Kevin Gloss <kgloss89@gmail.com>
2022-03-08 10:13:27 -0700

    Add destination forecast

--- app/services/forecast_service.rb
+++ app/services/forecast_service.rb
@@ -1,16 +1,27 @@
 class ForecastService < ApiService 
   class << self 
     def connection
-      response = Faraday.new(url: 'https://api.openweathermap.org/data/2.5/') do |f|
+      response = Faraday.new(url: 'https://api.openweathermap.org/data/2.5/onecall') do |f|
         f.params['appid'] = ENV['weather_key']
       end
     end
 
     def get_forecast(lat, lon)
-      response = connection.get('onecall') do |f|
+      response = connection.get do |f|
         f.params['lat'] = lat 
         f.params['lon'] = lon
         f.params['exclude'] = 'minutely,alerts'
+        f.params['units'] = 'imperial'
+      end
+
+      parse_json(response)
+    end
+
+    def destination_forecast(lat, lon)
+      response = connection.get do |f|
+        f.params['lat'] = lat 
+        f.params['lon'] = lon
+        f.params['exclude'] = 'minutely,alerts,current'
         f.params['units'] = 'imperial'
       end
 
